663437364f5ffc34f8a4140f6c243432d9d0251e
Toni Moreira <toni@itentecnologia.com.br>
2022-12-13 09:50:38 -0300

    Add include and exclude

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const fs = require('fs')
 const path = require('path')
 
 dotenv.config({path: './.env'})
+const filterBy = (strs, path) => {
+    for (const str of strs) {
+        if (new RegExp('^' + str.replace(/\*/g, '.*') + '$').test(path)) return true
+    }
+    return false
+}
 
 aws.config.update({
     region: process.env.AWS_REGION,
@@ -11,7 +17,6 @@ aws.config.update({
     secretAccessKey: process.env.AWS_SECRET_KEY,
 })
 const s3 = new aws.S3()
-
 
 async function uploadDir(s3Path, bucketName) {
     async function getFiles(dir) {
@@ -23,7 +28,16 @@ async function uploadDir(s3Path, bucketName) {
         return Array.prototype.concat(...files)
     }
 
-    const files = (await getFiles(s3Path))
+    var files = (await getFiles(s3Path))
+    console.log(files)
+    const include = process.env.INCLUDE?.split(';')
+    const exclude = process.env.EXCLUDE?.split(';')
+    if (include) {
+        files = files.filter(file => filterBy(include, file))
+    } else if (exclude) {
+        files = files.filter(file => !filterBy(exclude, file))
+    }
+
     const uploads = files.map((filePath) => s3.putObject({
         Key: process.env.DEST_PATH + '/' + path.relative(s3Path, filePath),
         Bucket: bucketName,
